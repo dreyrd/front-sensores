3e7dde43870a4ee48e798918ef8ea4fb251205b6
dreyrd <andreyrosadias8@gmail.com>
2024-12-05 11:28:36 -0300

    habilitado ao firebase

--- src/pages/tabelaSensores.jsx
+++ src/pages/tabelaSensores.jsx
@@ -7,41 +7,76 @@ import axios from "axios"
 
 export function TabelaSensores(){
 
-    const [token, setToken] = useState('')
+    const [token, setToken] = useState(localStorage.getItem('token'))
+    const [sensores, setSensores] = useState([])
 
     
 
-    useEffect(()=>{
-        try{
-            setToken(localStorage.getItem('token'))
-            console.log(token)
-        }
-        catch(error){
-            console.log(error)
-        }
-    }, [])
+    useEffect(() => {
+        const fetchSensores = async () => {
+          try {
+            const response = await axios.get("http://127.0.0.1:8000/api/sensor/", {
+                headers: { 
+                    Authorization: `Bearer ${token}` 
+                },
+            });
+            const sensoresDados = await Promise.all(
+              response.data.map(async (sensor) => {
+                try {
+                    const tipoResponse = await axios.get(`http://127.0.0.1:8000/api/tipos_sensor/${sensor.tipo}/`,
+                    { 
+                        headers: { 
+                            Authorization: `Bearer ${token}` 
+                        } 
+                    });
+
+                    return { 
+                        ...sensor, 
+                        tipo: tipoResponse.data.tipo 
+                    };
+                
+                } catch (error) {
+
+                    return { 
+                        ...sensor, 
+                        tipo: "Desconhecido" 
+                    }; 
+
+                }
+              })
+            );
+            setSensores(sensoresDados);
+          } catch (error) {
+
+            console.error("Erro ao buscar sensores:", error);
+
+          }
+        };
+    
+        fetchSensores();
+
+      }, [token]);
 
 
-    try{
-        const sensores = axios.get('http://127.0.0.1:8000/api/sensor/',
-        {
-            headers: {
-                Authorization: `Bearer ${token}`
-            }
-        })
-        .then(() => {
-            console.log(sensores)
-
-        })
-    }
-    catch(error){
-        console.log(error)
-    }
+    
 
     return(
-        <div className="w-full h-full flex flex-col gap-10 pt-10 px-20">
+        <div className="w-full h-full flex">
 
-            <RowCard id_sensor="1" localizacao="102" responsavel="Andrey" tipo="Temperatura" />
+            <div className="w-9/12 h-full flex flex-col gap-10 py-10 px-20 overflow-auto">
+
+
+                {sensores.map((sensor) => (
+
+                    <RowCard key={sensor.id} id_sensor={sensor.id} localizacao={sensor.localizacao} responsavel={sensor.responsavel} tipo={sensor.tipo} />
+                ))}
+
+            </div>
+
+            <div className="w-3/12 h-full bg-red-300">
+
+            </div>
+
 
         </div>
     )
