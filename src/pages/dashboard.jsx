d45775f08703dbe97467802ba2d0699bc38f8196
dreyrd <andreyrosadias8@gmail.com>
2024-12-16 11:28:48 -0300

    adicao de graficos

--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,29 +1,84 @@
 import ApexCharts from "apexcharts";
 
+import { useState } from "react";
+
+
 export function Dashboard(){
 
+          
     var options = {
+        series: [44, 55, 13, 33],
         chart: {
-            type: 'line'
-        },
-        series: [{
-            name: 'sales',
-            data: [30,40,35,50,49,60,70,91,125]
-        }],
-        xaxis: {
-            categories: [1991,1992,1993,1994,1995,1996,1997, 1998,1999]
+        width: 380,
+        type: 'donut',
+      },
+      dataLabels: {
+        enabled: false
+      },
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            show: false
+          }
         }
-        }
+      }],
+      legend: {
+        position: 'right',
+        offsetY: 0,
+        height: 230,
+      }
+      };
 
-    var chart = new ApexCharts(document.querySelector("#chart"), options);
-
-    chart.render();
+      var chart = new ApexCharts(document.querySelector("#chart"), options);
+      chart.render();
+    
+    
+      function appendData() {
+      var arr = chart.w.globals.series.slice()
+      arr.push(Math.floor(Math.random() * (100 - 1 + 1)) + 1)
+      return arr;
+    }
+    
+    function removeData() {
+      var arr = chart.w.globals.series.slice()
+      arr.pop()
+      return arr;
+    }
+    
+    function randomize() {
+      return chart.w.globals.series.map(function() {
+          return Math.floor(Math.random() * (100 - 1 + 1)) + 1
+      })
+    }
+    
+    function reset() {
+      return options.series
+    }
+    
+    // document.querySelector("#randomize").addEventListener("click", function() {
+    //   chart.updateSeries(randomize())
+    // })
+    
+    // document.querySelector("#add").addEventListener("click", function() {
+    //   chart.updateSeries(appendData())
+    // })
+    
+    // document.querySelector("#remove").addEventListener("click", function() {
+    //   chart.updateSeries(removeData())
+    // })
+    
+    // document.querySelector("#reset").addEventListener("click", function() {
+    //   chart.updateSeries(reset())
+    // })
 
     return(
         <div>
 
-            <p>a</p>
-
+            <div id="#chart" className="h-100 w-100"></div>
 
         </div>
     )
